137e0a6dd244574aa2acfbcfeb3f00a9b7f70987
sevicd <david.sevic@grenoble-inp.org>
2024-11-18 15:24:22 +0100

    Add: Finished force calculation

--- main.cpp
+++ main.cpp
@@ -2,33 +2,34 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
+#include <array>
 
 const double g = 6.67 * pow(10, -11);
 
 int main() {
-    int n = 2;
+    // parameters
+    const int n = 2;
+    const int n_dim = 2;
 
     // mass
 
-    double* masses = new double[n];
+    std::array<double, n> masses;
 
     double lower_m = 10.0;
     double higher_m = 20.0;
 
     // position
 
-    std::vector<double>* vectors = new std::vector<double>[n];
+    std::array<std::array<double, n_dim>, n> vectors;
 
     double lower_p = -1.0;
     double higher_p = 1.0;
-    int n_dim = 2;
 
     //std::srand(std::time(nullptr));
     for(int i = 0; i < n; ++i) {
         masses[i] = lower_m + static_cast<double>(std::rand()) / RAND_MAX * (higher_m - lower_m);
         for(int j = 0; j < n_dim; ++j)
-            vectors[i].push_back(lower_p + static_cast<double>(std::rand()) / RAND_MAX * (higher_p - lower_p));
+            vectors[i][j] = lower_p + static_cast<double>(std::rand()) / RAND_MAX * (higher_p - lower_p);
     }
     
     for(int i = 0; i < n; ++i) {
@@ -41,32 +42,38 @@ int main() {
     }
 
     // forces
-
-    double* forces = new double[n];
+    
+    std::array<std::array<double, n_dim>, n> force_vectors;
 
     for(int i = 0; i < n; ++i) {
-        double sum = 0;
+        // calculating the sum
+        std::array<double, n_dim> sum = {};
         for(int j = 0; j < n; ++j) {
             if(j == i)
                 continue;
-            double distance = 0;
+            // calculating the euclidian distance and displacement
+            double euclid_distance = 0;
+            std::array<double, n_dim> displacement;
             for(int k = 0; k < n_dim; ++k) {
-                distance += abs(vectors[i][k] - vectors[j][k]) NIJE DOBRO RACUNANJE EUCLEDIAN DISTANCE
-                std::cout<<"vectors[i][k]: "<<vectors[i][k]<<" vectors[j][k]: "<<vectors[j][k]<<" => abs(vectors[i][k] - vectors[j][k]): "<<abs(vectors[i][k] - vectors[j][k])<<std::endl;
+                euclid_distance += (vectors[i][k] - vectors[j][k]) * (vectors[i][k] - vectors[j][k]);
+                displacement[k] = vectors[i][k] - vectors[j][k];
             }
-            std::cout<<"dist: "<<distance<<std::endl;
-            sum += (masses[i] * masses[j] * distance) / abs(distance * distance * distance);
+            euclid_distance = sqrt(euclid_distance);
+            for(int k = 0; k < n_dim; ++k) {
+                sum[k] += (masses[i] * masses[j] * displacement[k]) / (euclid_distance * euclid_distance * euclid_distance);
+            }
         }
-        forces[i] = g * sum;
+        for(int k = 0; k < n_dim; ++k) {
+            force_vectors[i][k] = g * sum[k];
+        }
     }
 
     std::cout<<"forces:"<<std::endl;
     for(int i = 0; i < n; ++i) {
-        std::cout<<forces[i]<<std::endl;
+        std::cout<<"[  ";
+        for(int k = 0; k < n_dim; ++k){
+            std::cout<<force_vectors[i][k]<<' ';
+        }
+        std::cout<<']'<<std::endl;
     }
-    
-    // memory deallocation
-    delete[] masses;
-    delete[] vectors;
-    delete[] forces;
 }
